cd0abe899def0a6472c7034e1793b477ac560ac3
sungwo0 <70313948+sungwo0@users.noreply.github.com>
2025-03-11 00:56:55 +0900

    test

---
+++ 0304/usejsx/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, JSX!</h1>
+      <p>This is a JSX example.</p>
+    </div>
+  );
+}
+
+
+export default App;
