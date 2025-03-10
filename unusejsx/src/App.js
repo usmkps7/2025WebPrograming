9f37d80ca995c6da5ca5fda31329830d1a88338e
sungwo0 <70313948+sungwo0@users.noreply.github.com>
2025-03-11 00:45:11 +0900

    test

---
+++ unusejsx/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return React.createElement(
+    "div",
+    null,
+    React.createElement("h1", null, "Hello, JSX!"),
+    React.createElement("p", null, "This is a example without JSX.")
+  );
+}
+
+
+export default App;
