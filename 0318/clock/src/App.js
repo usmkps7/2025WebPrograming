bfae2c89b6a3d0a5b3f394046428043b7aaff883
sungwo0 <70313948+sungwo0@users.noreply.github.com>
2025-03-24 21:18:16 +0900

    web

---
+++ 0318/clock/src/App.js
@@ -0,0 +1,11 @@
+import Clock1 from "./chapter04/clock";
+
+function App() {
+  return (
+    <div>
+      <Clock1 />
+    </div>
+  );
+}
+
+export default App;
---
+++ 0318/component/src/comment/commentList.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import Comment from "./comment";
+
+const commentse = [
+    {
+        name: "이인제",
+        comment: "안녕하세요, 소플입니다.",
+    },
+    {
+        name: "유재석",
+        comment: "리액트 재미있어요~!",
+    },
+    {
+        name: "강인경",
+        comment: "저도 리액트 배워 보고 싶어요!!",
+    },
+];
+
+function CommentList(props){
+    return (
+        <div>
+            {commentse.map((comment) => {
+                return (
+                    <Comment name={comment.name} comment={comment.comment} />
+                )
+            })}
+        </div>
+    );
+}
+
+export default CommentList;
